28bd9d2a0767ac6bf000d0c17e51885de2a90475
Sberex <sergey301276@gmail.com>
2018-02-26 19:25:07 +0200

    Update event.go

--- accounts/abi/event.go
+++ accounts/abi/event.go
@@ -20,8 +20,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/crypto"
 )
 
 // Event is an event potentially triggered by the EVM's LOG mechanism. The Event
