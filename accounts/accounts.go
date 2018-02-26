46f939ff484702b006492920384301d14ac1c896
Sberex <sergey301276@gmail.com>
2018-02-26 19:18:30 +0200

    Update accounts.go

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -20,10 +20,10 @@ package accounts
 import (
 	"math/big"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
+	ethereum "github.com/Sberex/go-sberex"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/event"
 )
 
 // Account represents an Ethereum account located at a specific location defined
