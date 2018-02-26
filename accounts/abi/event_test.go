2ecc1c7404cc281c4d6d56c08dc2363a861d1096
Sberex <sergey301276@gmail.com>
2018-02-26 19:26:40 +0200

    Update event_test.go

--- accounts/abi/event_test.go
+++ accounts/abi/event_test.go
@@ -25,8 +25,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/crypto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
