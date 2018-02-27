7cc66ab2081599b1d15aec6c03e250762f4734b5
Sberex <sergey301276@gmail.com>
2018-02-27 20:44:48 +0200

    Update account_cache_test.go

--- accounts/keystore/account_cache_test.go
+++ accounts/keystore/account_cache_test.go
@@ -29,8 +29,8 @@ import (
 
 	"github.com/cespare/cp"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/Sberex/go-sberex/accounts"
+	"github.com/Sberex/go-sberex/common"
 )
 
 var (
