50184d200b1640c02d0b1f1285c6c1f36c05c0c6
Sberex <sergey301276@gmail.com>
2018-02-27 20:52:01 +0200

    Update keystore_plain_test.go

--- accounts/keystore/keystore_plain_test.go
+++ accounts/keystore/keystore_plain_test.go
@@ -27,8 +27,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/crypto"
 )
 
 func tmpKeyStoreIface(t *testing.T, encrypted bool) (dir string, ks keyStore) {
