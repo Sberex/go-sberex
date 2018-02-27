5f23ff05066cd4a794d13dd278a54c52030d685e
Sberex <sergey301276@gmail.com>
2018-02-27 20:54:57 +0200

    Update presale.go

--- accounts/keystore/presale.go
+++ accounts/keystore/presale.go
@@ -25,8 +25,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/accounts"
+	"github.com/Sberex/go-sberex/crypto"
 	"github.com/pborman/uuid"
 	"golang.org/x/crypto/pbkdf2"
 )
