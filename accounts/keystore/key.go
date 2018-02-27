2b039f9d5ad3692141d549be60870038fd258f30
Sberex <sergey301276@gmail.com>
2018-02-27 20:47:26 +0200

    Update key.go

--- accounts/keystore/key.go
+++ accounts/keystore/key.go
@@ -29,9 +29,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/accounts"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/crypto"
 	"github.com/pborman/uuid"
 )
 
