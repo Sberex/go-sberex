2bc762f0e77204772ac179a6b5f0eeac9569f7d5
Sberex <sergey301276@gmail.com>
2018-02-27 20:53:53 +0200

    Update keystore_wallet.go

--- accounts/keystore/keystore_wallet.go
+++ accounts/keystore/keystore_wallet.go
@@ -19,9 +19,9 @@ package keystore
 import (
 	"math/big"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/core/types"
+	ethereum "github.com/Sberex/go-sberex"
+	"github.com/Sberex/go-sberex/accounts"
+	"github.com/Sberex/go-sberex/core/types"
 )
 
 // keystoreWallet implements the accounts.Wallet interface for the original
