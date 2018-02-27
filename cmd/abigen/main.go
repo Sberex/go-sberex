64b07a947d8880498666bf935fdebc54b4fe5175
Sberex <sergey301276@gmail.com>
2018-02-27 21:35:38 +0200

    Update main.go

--- cmd/abigen/main.go
+++ cmd/abigen/main.go
@@ -24,8 +24,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common/compiler"
+	"github.com/Sberex/go-sberex/accounts/abi/bind"
+	"github.com/Sberex/go-sberex/common/compiler"
 )
 
 var (
