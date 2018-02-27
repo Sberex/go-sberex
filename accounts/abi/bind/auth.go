7513167078d773f80cfa19fffa33befa8de26404
Sberex <sergey301276@gmail.com>
2018-02-27 14:45:47 +0200

    Update auth.go

--- accounts/abi/bind/auth.go
+++ accounts/abi/bind/auth.go
@@ -22,10 +22,10 @@ import (
 	"io"
 	"io/ioutil"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/accounts/keystore"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/crypto"
 )
 
 // NewTransactor is a utility method to easily create a transaction signer from
