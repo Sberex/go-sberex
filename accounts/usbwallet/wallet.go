613c5b55c6e7bcbcb7809451b7f587e48048a2ce
Sberex <sergey301276@gmail.com>
2018-02-27 21:04:25 +0200

    Update wallet.go

--- accounts/usbwallet/wallet.go
+++ accounts/usbwallet/wallet.go
@@ -26,10 +26,10 @@ import (
 	"time"
 
 	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Sberex/go-sberex/accounts"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/log"
 	"github.com/karalabe/hid"
 )
 
