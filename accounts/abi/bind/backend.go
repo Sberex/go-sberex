509e9e0b0e7a74385e13efeaac5a186f61fc0386
Sberex <sergey301276@gmail.com>
2018-02-27 14:49:31 +0200

    Update backend.go

--- accounts/abi/bind/backend.go
+++ accounts/abi/bind/backend.go
@@ -21,9 +21,9 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Sberex/go-sberex"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core/types"
 )
 
 var (
