d217730d9b38e9924aa92cbe424d34977cc65645
Sberex <sergey301276@gmail.com>
2018-02-27 14:54:05 +0200

    Update topics.go

--- accounts/abi/bind/topics.go
+++ accounts/abi/bind/topics.go
@@ -22,9 +22,9 @@ import (
 	"math/big"
 	"reflect"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/accounts/abi"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/crypto"
 )
 
 // makeTopics converts a filter query argument list into a filter topic set.
