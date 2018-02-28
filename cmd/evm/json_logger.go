b347dd4951a4a7d59f20429dbdf5fd545089b91e
Sberex <sergey301276@gmail.com>
2018-02-28 11:56:59 +0200

    Update json_logger.go

--- cmd/evm/json_logger.go
+++ cmd/evm/json_logger.go
@@ -22,9 +22,9 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/math"
+	"github.com/Sberex/go-sberex/core/vm"
 )
 
 type JSONLogger struct {
