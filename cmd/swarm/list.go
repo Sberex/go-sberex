80f542ab5b599d08308f1897abf50f28f2ea06b0
Sberex <sergey301276@gmail.com>
2018-02-28 17:56:06 +0200

    Update list.go

--- cmd/swarm/list.go
+++ cmd/swarm/list.go
@@ -22,8 +22,8 @@ import (
 	"strings"
 	"text/tabwriter"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	swarm "github.com/ethereum/go-ethereum/swarm/api/client"
+	"github.com/Sberex/go-sberex/cmd/utils"
+	swarm "github.com/Sberex/go-sberex/swarm/api/client"
 	"gopkg.in/urfave/cli.v1"
 )
 
