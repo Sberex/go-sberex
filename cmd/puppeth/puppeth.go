d6d9c177734348c6852fc5c5ea4888bb1e3f0f03
Sberex <sergey301276@gmail.com>
2018-02-28 17:21:48 +0200

    Update puppeth.go

--- cmd/puppeth/puppeth.go
+++ cmd/puppeth/puppeth.go
@@ -22,7 +22,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Sberex/go-sberex/log"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -30,7 +30,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "puppeth"
-	app.Usage = "assemble and maintain private Ethereum networks"
+	app.Usage = "assemble and maintain private Sberex networks"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "network",
