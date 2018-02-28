a3375e6d668fc6b41eca990e464c9cef0933a722
Sberex <sergey301276@gmail.com>
2018-02-28 16:59:21 +0200

    Update misccmd.go

--- cmd/geth/misccmd.go
+++ cmd/geth/misccmd.go
@@ -23,10 +23,10 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/eth"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/cmd/utils"
+	"github.com/Sberex/go-sberex/consensus/ethash"
+	"github.com/Sberex/go-sberex/eth"
+	"github.com/Sberex/go-sberex/params"
 	"gopkg.in/urfave/cli.v1"
 )
 
