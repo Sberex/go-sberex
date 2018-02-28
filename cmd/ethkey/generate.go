41138882d05e09e7599784f0d4462cbe4ce88187
Sberex <sergey301276@gmail.com>
2018-02-28 11:32:27 +0200

    Update generate.go

--- cmd/ethkey/generate.go
+++ cmd/ethkey/generate.go
@@ -23,9 +23,9 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/accounts/keystore"
+	"github.com/Sberex/go-sberex/cmd/utils"
+	"github.com/Sberex/go-sberex/crypto"
 	"github.com/pborman/uuid"
 	"gopkg.in/urfave/cli.v1"
 )
