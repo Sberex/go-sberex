c75d52f39f46d730ab55cb69952318cb6ef7d00b
Sberex <sergey301276@gmail.com>
2018-02-28 17:54:07 +0200

    Update config_test.go

--- cmd/swarm/config_test.go
+++ cmd/swarm/config_test.go
@@ -25,9 +25,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/swarm"
-	"github.com/ethereum/go-ethereum/swarm/api"
+	"github.com/Sberex/go-sberex/rpc"
+	"github.com/Sberex/go-sberex/swarm"
+	"github.com/Sberex/go-sberex/swarm/api"
 
 	"github.com/docker/docker/pkg/reexec"
 )
