067df5274bedc156d262c17ae5711229e1d8303f
Sberex <sergey301276@gmail.com>
2018-02-28 17:29:19 +0200

    Update wizard_genesis.go

--- cmd/puppeth/wizard_genesis.go
+++ cmd/puppeth/wizard_genesis.go
@@ -25,10 +25,10 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core"
+	"github.com/Sberex/go-sberex/log"
+	"github.com/Sberex/go-sberex/params"
 )
 
 // makeGenesis creates a new genesis struct based on some user input.
