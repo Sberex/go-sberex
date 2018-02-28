877b0318d99ec93f7feafdb57f072fd8daa75cd9
Sberex <sergey301276@gmail.com>
2018-02-28 17:24:37 +0200

    Update wizard.go

--- cmd/puppeth/wizard.go
+++ cmd/puppeth/wizard.go
@@ -30,9 +30,9 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core"
+	"github.com/Sberex/go-sberex/log"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
