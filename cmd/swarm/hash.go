fcf6006f92c2e0ad789bb1a35b11945d3c237898
Sberex <sergey301276@gmail.com>
2018-02-28 17:55:28 +0200

    Update hash.go

--- cmd/swarm/hash.go
+++ cmd/swarm/hash.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Sberex/go-sberex/cmd/utils"
+	"github.com/Sberex/go-sberex/swarm/storage"
 	"gopkg.in/urfave/cli.v1"
 )
 
