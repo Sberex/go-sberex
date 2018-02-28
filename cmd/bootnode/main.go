f812aa8a487a203752ce3a74206451a0262611d1
Sberex <sergey301276@gmail.com>
2018-02-28 11:28:04 +0200

    Update main.go

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -24,13 +24,13 @@ import (
 	"net"
 	"os"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/p2p/discover"
-	"github.com/ethereum/go-ethereum/p2p/discv5"
-	"github.com/ethereum/go-ethereum/p2p/nat"
-	"github.com/ethereum/go-ethereum/p2p/netutil"
+	"github.com/Sberex/go-sberex/cmd/utils"
+	"github.com/Sberex/go-sberex/crypto"
+	"github.com/Sberex/go-sberex/log"
+	"github.com/Sberex/go-sberex/p2p/discover"
+	"github.com/Sberex/go-sberex/p2p/discv5"
+	"github.com/Sberex/go-sberex/p2p/nat"
+	"github.com/Sberex/go-sberex/p2p/netutil"
 )
 
 func main() {
