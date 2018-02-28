9165824be6743df5046021a3191f047c48918aba
Sberex <sergey301276@gmail.com>
2018-02-28 17:54:56 +0200

    Update db.go

--- cmd/swarm/db.go
+++ cmd/swarm/db.go
@@ -22,9 +22,9 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Sberex/go-sberex/cmd/utils"
+	"github.com/Sberex/go-sberex/log"
+	"github.com/Sberex/go-sberex/swarm/storage"
 	"gopkg.in/urfave/cli.v1"
 )
 
