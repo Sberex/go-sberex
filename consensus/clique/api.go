4fc07a3527709cc37f5100d43bc05f84b1d0e464
Sberex <sergey301276@gmail.com>
2018-03-01 09:54:55 +0200

    Update api.go

--- consensus/clique/api.go
+++ consensus/clique/api.go
@@ -17,10 +17,10 @@
 package clique
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/consensus"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/rpc"
 )
 
 // API is a user facing RPC API to allow controlling the signer and voting
