685b6394110ede4735a0298cb91bfb09892425f2
Sberex <sergey301276@gmail.com>
2018-03-01 10:01:35 +0200

    Update snapshot.go

--- consensus/clique/snapshot.go
+++ consensus/clique/snapshot.go
@@ -20,10 +20,10 @@ import (
 	"bytes"
 	"encoding/json"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/ethdb"
+	"github.com/Sberex/go-sberex/params"
 	lru "github.com/hashicorp/golang-lru"
 )
 
