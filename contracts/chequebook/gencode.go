a3151de628d96cc8d786bb1a32ca05a7e624cd8a
Sberex <sergey301276@gmail.com>
2018-03-01 16:09:38 +0200

    Update gencode.go

--- contracts/chequebook/gencode.go
+++ contracts/chequebook/gencode.go
@@ -25,11 +25,11 @@ import (
 	"io/ioutil"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
-	"github.com/ethereum/go-ethereum/contracts/chequebook/contract"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Sberex/go-sberex/accounts/abi/bind"
+	"github.com/Sberex/go-sberex/accounts/abi/bind/backends"
+	"github.com/Sberex/go-sberex/contracts/chequebook/contract"
+	"github.com/Sberex/go-sberex/core"
+	"github.com/Sberex/go-sberex/crypto"
 )
 
 var (
