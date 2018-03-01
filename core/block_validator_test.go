24ddac708bfe56fee6cfde7b3cd78f5ad94bdbbf
Sberex <sergey301276@gmail.com>
2018-03-01 19:22:07 +0200

    Update block_validator_test.go

--- core/block_validator_test.go
+++ core/block_validator_test.go
@@ -21,11 +21,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/consensus/ethash"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/core/vm"
+	"github.com/Sberex/go-sberex/ethdb"
+	"github.com/Sberex/go-sberex/params"
 )
 
 // Tests that simple header verification works, for both good and bad blocks.
