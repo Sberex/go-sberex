f69b21ee2459140337c5f3053eabf1767610f3d4
Sberex <sergey301276@gmail.com>
2018-03-01 10:16:23 +0200

    Update dao.go

--- consensus/misc/dao.go
+++ consensus/misc/dao.go
@@ -21,9 +21,9 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/core/state"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/params"
 )
 
 var (
