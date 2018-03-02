e715cbd3d7d990ba4597738817c6ff0a9fb0482b
Sberex <sergey301276@gmail.com>
2018-03-02 20:48:29 +0200

    Update gen_header_json.go

--- core/types/gen_header_json.go
+++ core/types/gen_header_json.go
@@ -7,8 +7,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/hexutil"
 )
 
 var _ = (*headerMarshaling)(nil)
