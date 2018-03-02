55cc2dd4216c53523321a1f50eaccc470fd7446b
Sberex <sergey301276@gmail.com>
2018-03-02 20:49:18 +0200

    Update gen_receipt_json.go

--- core/types/gen_receipt_json.go
+++ core/types/gen_receipt_json.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"errors"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/hexutil"
 )
 
 var _ = (*receiptMarshaling)(nil)
