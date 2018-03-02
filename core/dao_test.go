0c5dcb2fb4137f1986c6070c21f284b2c47506d6
Sberex <sergey301276@gmail.com>
2018-03-02 09:06:19 +0200

    Update dao_test.go

--- core/dao_test.go
+++ core/dao_test.go
@@ -20,10 +20,10 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/consensus/ethash"
+	"github.com/Sberex/go-sberex/core/vm"
+	"github.com/Sberex/go-sberex/ethdb"
+	"github.com/Sberex/go-sberex/params"
 )
 
 // Tests that DAO-fork enabled clients can properly filter out fork-commencing
