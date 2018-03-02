b15f133604378b8469934806a56eab3ccb5bdddb
Sberex <sergey301276@gmail.com>
2018-03-02 21:09:55 +0200

    Update gen_structlog.go

--- core/vm/gen_structlog.go
+++ core/vm/gen_structlog.go
@@ -6,9 +6,9 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/hexutil"
+	"github.com/Sberex/go-sberex/common/math"
 )
 
 var _ = (*structLogMarshaling)(nil)
