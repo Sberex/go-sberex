8216d7b6ff7878225415e4cddd5c8f51bed5a0cb
Sberex <sergey301276@gmail.com>
2018-03-02 21:11:40 +0200

    Update instructions.go

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -21,11 +21,11 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/math"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/crypto"
+	"github.com/Sberex/go-sberex/params"
 )
 
 var (
