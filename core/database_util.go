73a4446328141ba777a514f3f99ea13f4b7f2d2c
Sberex <sergey301276@gmail.com>
2018-03-02 09:36:27 +0200

    Update database_util.go

--- core/database_util.go
+++ core/database_util.go
@@ -1,8 +1,8 @@
-// This file is part of the go-sberex library forked from go-ethereum 
-// repository. The go-sberex library is free software: you can redistribute 
-// it and/or modify it under the terms of the GNU Lesser General Public 
-// License as published by the Free Software Foundation, either version 3 
-// of the License, or (at your option) any later version.
+// This file is part of the go-sberex library. The go-sberex library is 
+// free software: you can redistribute it and/or modify it under the terms 
+// of the GNU Lesser General Public License as published by the Free 
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
 //
 // The go-sberex library is distributed in the hope that it will be useful, 
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
