7c26216ea97c69e82fc15996a88b6bba619c19aa
Sberex <sergey301276@gmail.com>
2018-03-02 10:35:54 +0200

    Update gen_genesis_account.go

--- core/gen_genesis_account.go
+++ core/gen_genesis_account.go
@@ -7,9 +7,9 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/hexutil"
+	"github.com/Sberex/go-sberex/common/math"
 )
 
 var _ = (*genesisAccountMarshaling)(nil)
