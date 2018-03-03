d22ce204cad4b00bec9ab5d456d4d1091b7b68ee
Sberex <sergey301276@gmail.com>
2018-03-03 16:13:57 +0200

    Update protocol_test.go

--- eth/protocol_test.go
+++ eth/protocol_test.go
@@ -22,12 +22,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/eth/downloader"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/core/types"
+	"github.com/Sberex/go-sberex/crypto"
+	"github.com/Sberex/go-sberex/eth/downloader"
+	"github.com/Sberex/go-sberex/p2p"
+	"github.com/Sberex/go-sberex/rlp"
 )
 
 func init() {
