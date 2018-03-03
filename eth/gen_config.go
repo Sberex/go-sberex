5c3dfa801f6ee5e2ad0eea2e2f1f7fc91787686e
Sberex <sergey301276@gmail.com>
2018-03-03 16:03:18 +0200

    Update gen_config.go

--- eth/gen_config.go
+++ eth/gen_config.go
@@ -5,12 +5,12 @@ package eth
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/eth/downloader"
-	"github.com/ethereum/go-ethereum/eth/gasprice"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/hexutil"
+	"github.com/Sberex/go-sberex/consensus/ethash"
+	"github.com/Sberex/go-sberex/core"
+	"github.com/Sberex/go-sberex/eth/downloader"
+	"github.com/Sberex/go-sberex/eth/gasprice"
 )
 
 var _ = (*configMarshaling)(nil)
