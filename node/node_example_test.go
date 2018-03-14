87d516d9f409cba92cf9d87ec1e93bc25a78202b
Sberex <sergey301276@gmail.com>
2018-03-14 09:13:13 +0200

    Update node_example_test.go

--- node/node_example_test.go
+++ node/node_example_test.go
@@ -20,9 +20,9 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/Sberex/go-sberex/node"
+	"github.com/Sberex/go-sberex/p2p"
+	"github.com/Sberex/go-sberex/rpc"
 )
 
 // SampleService is a trivial network service that can be attached to a node for
