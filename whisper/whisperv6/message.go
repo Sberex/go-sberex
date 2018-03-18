4a1759cfe65661e05c1d6374ed128ec154f18155
Sberex <sergey301276@gmail.com>
2018-03-18 15:46:25 +0200

    Update message.go

--- whisper/whisperv6/message.go
+++ whisper/whisperv6/message.go
@@ -28,10 +28,10 @@ import (
 	mrand "math/rand"
 	"strconv"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/ecies"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/crypto"
+	"github.com/Sberex/go-sberex/crypto/ecies"
+	"github.com/Sberex/go-sberex/log"
 )
 
 // MessageParams specifies the exact way a message should be wrapped
