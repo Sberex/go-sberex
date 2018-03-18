91b3e3afda0d89b135ed554d1ffe45a7739d50d0
Sberex <sergey301276@gmail.com>
2018-03-18 07:29:07 +0200

    Update gen_stenv.go

--- tests/gen_stenv.go
+++ tests/gen_stenv.go
@@ -7,8 +7,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/Sberex/go-sberex/common"
+	"github.com/Sberex/go-sberex/common/math"
 )
 
 var _ = (*stEnvMarshaling)(nil)
