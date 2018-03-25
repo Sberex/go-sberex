6133700b0cd5a94448fd80255d0f12d437d29e77
Sberex <sergey301276@gmail.com>
2018-03-25 19:54:20 +0300

    Update bugcmd.go

--- cmd/geth/bugcmd.go
+++ cmd/geth/bugcmd.go
@@ -38,7 +38,7 @@ var bugCommand = cli.Command{
 
 const issueUrl = "https://github.com/Sberex/go-sberex/issues/new"
 
-// reportBug reports a bug by opening a new URL to the go-ethereum GH issue
+// reportBug reports a bug by opening a new URL to the go-sberex GH issue
 // tracker and setting default values as the issue body.
 func reportBug(ctx *cli.Context) error {
 	// execute template and write contents to buff
