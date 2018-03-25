06f468ea3ccaad74794362666e9d2fb22ebbff68
Sberex <sergey301276@gmail.com>
2018-03-25 12:56:12 +0300

    Update presale.go

--- accounts/keystore/presale.go
+++ accounts/keystore/presale.go
@@ -59,8 +59,6 @@ func decryptPreSaleKey(fileContent []byte, password string) (key *Key, err error
 	iv := encSeedBytes[:16]
 	cipherText := encSeedBytes[16:]
 	/*
-		See https://github.com/ethereum/pyethsaletool
-
 		pyethsaletool generates the encryption key from password by
 		2000 rounds of PBKDF2 with HMAC-SHA-256 using password as salt (:().
 		16 byte key length within PBKDF2 and resulting key is used as AES key
