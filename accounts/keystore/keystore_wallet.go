0386bf2f3b40c8e7b3c4b6952222eca9ea3b7de1
Sberex <sergey301276@gmail.com>
2018-03-25 09:19:56 +0300

    Update keystore_wallet.go

--- accounts/keystore/keystore_wallet.go
+++ accounts/keystore/keystore_wallet.go
@@ -14,7 +14,7 @@ package keystore
 import (
 	"math/big"
 
-	ethereum "github.com/Sberex/go-sberex"
+	sberex "github.com/Sberex/go-sberex"
 	"github.com/Sberex/go-sberex/accounts"
 	"github.com/Sberex/go-sberex/core/types"
 )
@@ -71,7 +71,7 @@ func (w *keystoreWallet) Derive(path accounts.DerivationPath, pin bool) (account
 
 // SelfDerive implements accounts.Wallet, but is a noop for plain wallets since
 // there is no notion of hierarchical account derivation for plain keystore accounts.
-func (w *keystoreWallet) SelfDerive(base accounts.DerivationPath, chain ethereum.ChainStateReader) {}
+func (w *keystoreWallet) SelfDerive(base accounts.DerivationPath, chain sberex.ChainStateReader) {}
 
 // SignHash implements accounts.Wallet, attempting to sign the given hash with
 // the given account. If the wallet does not wrap this particular account, an
