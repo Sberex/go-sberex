abfa304bc75e8ffd5208897a35c7a0cbb1bb6eaf
Sberex <sergey301276@gmail.com>
2018-03-26 15:11:25 +0300

    Update wizard_node.go

--- cmd/puppeth/wizard_node.go
+++ cmd/puppeth/wizard_node.go
@@ -159,7 +159,7 @@ func (w *wizard) deployNode(boot bool) {
 		nocache = w.readDefaultString("n") != "n"
 	}
 	if out, err := deployNode(client, w.network, w.conf.bootnodes, infos, nocache); err != nil {
-		log.Error("Failed to deploy Ethereum node container", "err", err)
+		log.Error("Failed to deploy Sberex node container", "err", err)
 		if len(out) > 0 {
 			fmt.Printf("%s\n", out)
 		}
