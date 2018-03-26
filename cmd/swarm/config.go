33f9357682eed0c56daf876cc111114ca6d01dbe
Sberex <sergey301276@gmail.com>
2018-03-26 16:02:22 +0300

    Update config.go

--- cmd/swarm/config.go
+++ cmd/swarm/config.go
@@ -79,7 +79,7 @@ var tomlSettings = toml.Config{
 	MissingField: func(rt reflect.Type, field string) error {
 		link := ""
 		if unicode.IsUpper(rune(rt.Name()[0])) && rt.PkgPath() != "main" {
-			link = fmt.Sprintf(", check github.com/ethereum/go-ethereum/swarm/api/config.go for available fields")
+			link = fmt.Sprintf(", check github.com/Sberex/go-sberex/swarm/api/config.go for available fields")
 		}
 		return fmt.Errorf("field '%s' is not defined in %s%s", field, rt.String(), link)
 	},
