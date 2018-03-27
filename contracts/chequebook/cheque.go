6a358583f094cb58a73f26c9f3f7d72eafb5696b
Sberex <sergey301276@gmail.com>
2018-03-27 22:47:57 +0300

    Update cheque.go

--- contracts/chequebook/cheque.go
+++ contracts/chequebook/cheque.go
@@ -12,8 +12,8 @@
 // Package chequebook package wraps the 'chequebook' Sberex smart contract.
 //
 // The functions in this package allow using chequebook for
-// issuing, receiving, verifying cheques in ether; (auto)cashing cheques in ether
-// as well as (auto)depositing ether to the chequebook contract.
+// issuing, receiving, verifying cheques in sbr; (auto)cashing cheques in sbr
+// as well as (auto)depositing sbr to the chequebook contract.
 package chequebook
 
 //go:generate abigen --sol contract/chequebook.sol --exc contract/mortal.sol:mortal,contract/owned.sol:owned --pkg contract --out contract/chequebook.go
@@ -47,8 +47,8 @@ import (
 // Some functionality requires interacting with the blockchain:
 // * setting current balance on peer's chequebook
 // * sending the transaction to cash the cheque
-// * depositing ether to the chequebook
-// * watching incoming ether
+// * depositing sbr to the chequebook
+// * watching incoming sbr
 
 var (
 	gasToCash = uint64(2000000) // gas cost of a cash transaction using chequebook
