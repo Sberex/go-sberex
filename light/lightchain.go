71ea2c827245a38e6f7c999cc48e3b486ca08819
Sberex <sergey301276@gmail.com>
2018-03-27 09:03:51 +0300

    Update lightchain.go

--- light/lightchain.go
+++ light/lightchain.go
@@ -67,7 +67,7 @@ type LightChain struct {
 }
 
 // NewLightChain returns a fully initialised light chain using information
-// available in the database. It initialises the default Ethereum header
+// available in the database. It initialises the default Sberex header
 // validator.
 func NewLightChain(odr OdrBackend, config *params.ChainConfig, engine consensus.Engine) (*LightChain, error) {
 	bodyCache, _ := lru.New(bodyCacheLimit)
