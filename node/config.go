2193b6ace989344cba8b8bbef9bcd8d70087de41
Sberex <sergey301276@gmail.com>
2018-03-27 18:29:30 +0300

    Update config.go

--- node/config.go
+++ node/config.go
@@ -400,7 +400,7 @@ func makeAccountManager(conf *Config) (*accounts.Manager, string, error) {
 	var ephemeral string
 	if keydir == "" {
 		// There is no datadir.
-		keydir, err = ioutil.TempDir("", "go-ethereum-keystore")
+		keydir, err = ioutil.TempDir("", "go-sberex-keystore")
 		ephemeral = keydir
 	}
 
